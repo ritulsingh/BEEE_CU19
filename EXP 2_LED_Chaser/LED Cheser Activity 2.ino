a6dbde8958df68dba9f1eb2002c7657fc520577e
Ritul Singh <53478620+lutir05@users.noreply.github.com>
2019-09-21 01:02:22 +0530

    Add files via upload

---
+++ EXP 2_LED_Chaser/LED Cheser Activity 2.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(2, HIGH);
+  digitalWrite(1,LOW);
+  delay(1000);
+  digitalWrite(3,HIGH);
+  digitalWrite(2,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  delay(1000);
+}
